9657956d4eed7a7c41155561d042658f9f051f36
DevMania <devmania1223@gmail.com>
2022-08-18 23:26:28 +0800

    upload

---
+++ src/components/PageHeader/types.ts
@@ -0,0 +1,5 @@
+import { BoxProps } from '@zoinks-swap/uikit'
+
+export interface PageHeaderProps extends BoxProps {
+  background?: string
+}
---
+++ src/utils/calls/index.ts
@@ -0,0 +1,5 @@
+export * from './estimateGas'
+export * from './farms'
+export * from './pools'
+export * from './zoinksMint'
+export * from './snacksMint'
